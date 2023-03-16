0b107d8eee49a8baeaeb1fb116a9516d8da8fc83
Aryan Kasliwal <aryankasliwal@CPN-MAC-M1-001.local>
2023-03-16 16:45:00 +0800

    some

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import FileUploadBox from './Components/FileUploadBox';
 import React from 'react';
+import Result from './Components/Result'
 
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
         </header>
         <body className='body'>
           <FileUploadBox fileType = "MIDI"/>
+          <Result></Result>
         </body>
         <footer className='footer'>
           This is the Final Year Project by Aryan Girish Kasliwal.

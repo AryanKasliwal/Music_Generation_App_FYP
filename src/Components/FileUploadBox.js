8e1710f7ab679fbc2451503e520f951722169ba5
KASLIWAL Aryan Girish <akasliwal2@ad.cityu.edu.hk>
2023-04-06 11:06:20 +0800

    Added formdata

--- src/Components/FileUploadBox.js
+++ src/Components/FileUploadBox.js
@@ -20,10 +20,10 @@ class FileUploadBox extends React.Component {
     uploadFile(e) {
         let file = this.state.selectedFile;
         const formData = new FormData();
-        formData.append("File", file);
+        formData.append("file", file);
 
         axios
-        .post("http://localhost:8080/", file)
+        .post("http://127.0.0.1:8080/upload", formData)
         .then(res => console.log(res.data))
         .catch(error => console.warn(error));
     }

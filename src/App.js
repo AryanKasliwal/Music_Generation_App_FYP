76d9af5ee4a956cc332180a579bee12192e03735
Aryan Kasliwal <aryankasliwal@CPN-MAC-M1-001.local>
2023-04-08 13:14:47 +0800

    Added model selection

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import './Styles/App.css'
 import FileUploadBox from './Components/FileUploadBox';
 import React, { useState } from 'react';
-import ResultBox from './Components/ResultBox'
+import ResultBox from './Components/ResultBox';
+import ModelSelection from './Components/ModelSelection';
 
 
 function App() {
   const [generatedFileNames, setGeneratedFileNames] = useState([])
+  const [selectedModel, setSelectedModel] = useState("")
+
+  const updateSelectedModel = (choice) => {
+    setSelectedModel(choice)
+  }
 
   const updateFileNames = (data) => {
     setGeneratedFileNames(data)
+  }
+
+  const modelDescriptionHeadings = {
+    "": "Please select a model above",
+    "RNN": "Capabilities of RNN",
+    "VQ-VAE": "Description of chosen model"
+  }
+
+  const modelDescriptions = {
+    "": "",
+    "RNN": "You have selected a RNN. This model can generate MIDI sequences of various styles of drumming, such as rock, jazz, and funk, and can be trained on any dataset of MIDI drum tracks. This RNN uses long short-term memory (LSTM) RNNs to learn the patterns and structures of drum beats from the training data. It also employs an attention mechanism that allows the model to focus on different parts of the drum track while generating new beats.",
+    "VQ-VAE": ""
   }
   return (
     <div className='App'>
       <header className='App-header'>
         AI Music Generation
       </header>
-    {/* //     <div className='audio-player'>
-            
-    //       </div> */}
-        <body className='body'>
-          <div className='model-description'>
-            <h1 className='desc-heading'>Description of chosen model</h1>
-            <p>
-              You have selected a RNN. This model can generate MIDI sequences of various styles of drumming, such as rock, jazz, and funk, and can be trained on any dataset of MIDI drum tracks. 
-              This RNN uses long short-term memory (LSTM) RNNs to learn the patterns and structures of drum beats from the training data. It also employs an attention mechanism that allows the model to focus on different parts of the drum track while generating new beats.
-            </p>
-          </div>
-          <div className='upper-body'>
-            <FileUploadBox onFileNames={updateFileNames}/>
-            {generatedFileNames.length > 0 && <ResultBox fileNames={generatedFileNames}></ResultBox>}
-          </div>
-        </body>
+      <div className='audio-player'>
+        <ModelSelection setModelChoice={updateSelectedModel}/>
+      </div>
+      <body className='body'>
+        <div className='model-description'>
+          <h1 className='desc-heading'>{modelDescriptionHeadings[selectedModel]}</h1>
+          <p>
+            {modelDescriptions[selectedModel]}
+          </p>
+        </div>
+        <div className='upper-body'>
+          <FileUploadBox onFileNames={updateFileNames}/>
+          {generatedFileNames.length > 0 && <ResultBox fileNames={generatedFileNames}></ResultBox>}
+        </div>
+      </body>
       <footer className='footer'>
         This is the Final Year Project by Aryan Girish Kasliwal.
         For academic purposes only.
--- src/Components/ModelSelection.js
+++ src/Components/ModelSelection.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../Styles/ModelSelection.css"
 
 export function ModelSelection(props) {
+  
+  // const [selectedOption, setSelectedOption] = useState('');
+
+  const handleChange = (event) => {
+    props.setModelChoice(event.target.value);
+  };
+
   return (
     <div className="top-section">
-      
+      <div>
+        <h2 className='dropdown-header'>Select a model to generate music</h2>
+      </div>
+      <select className='dropdown' id="dropdown-select" onChange={handleChange}>
+        <option value="">--Please choose an option--</option>
+        <option value="RNN">RNN</option>
+        <option value="VQ-VAE">VQ-VAE</option>
+      </select>
     </div>
   )
 }
